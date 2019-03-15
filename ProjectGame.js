7925e936cae51d6bd0734c40072a23f9a56b8909
bewza23114 <38246197+bewza23114@users.noreply.github.com>
2019-03-15 16:14:58 +0700

    Update ProjectGame.js

--- ProjectGame.js
+++ ProjectGame.js
@@ -170,11 +170,11 @@ $(function () {
         if (inputname == "") {
             console.log("blank");
             inputname = "[ไม่ระบุชื่อ]";
-        } else if (inputname.charAt(0) == "<"||inputname.charAt(1) == "<"||inputname.charAt(2) == "<"||inputname.charAt(3) == "<"||inputname.charAt(4) == "<"||inputname.charAt(5) == "<"
-        ||inputname.charAt(6) == "<"||inputname.charAt(7) == "<"||inputname.charAt(8) == "<"||inputname.charAt(9) == "<") {
+        } else if (inputname.charAt(0) == "<" || inputname.charAt(1) == "<" || inputname.charAt(2) == "<" || inputname.charAt(3) == "<" || inputname.charAt(4) == "<" || inputname.charAt(5) == "<"
+            || inputname.charAt(6) == "<" || inputname.charAt(7) == "<" || inputname.charAt(8) == "<" || inputname.charAt(9) == "<") {
             alert("ฮันแน่!!");
             location.reload();
-        } else if(inputname.length > 10){
+        } else if (inputname.length > 10) {
             console.log("length")
             alert("กรอกชื่อเล่นก็พอ");
             location.reload();
